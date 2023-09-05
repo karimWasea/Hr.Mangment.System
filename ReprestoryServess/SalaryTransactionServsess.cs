b7c20a8ec86d40dbaf506e33ff621ef2a4ad3439
karimWasea <131551998+karimWasea@users.noreply.github.com>
2023-09-06 00:15:55 +0300

    m kl

--- ReprestoryServess/SalaryTransactionServsess.cs
+++ ReprestoryServess/SalaryTransactionServsess.cs
@@ -25,9 +25,9 @@ namespace ReprestoryServess
 
         private ApplicationDBcontext _Context;
         private SalaryclackServesses _SalaryclackServesses;
-        public SalaryTransactionServsess(ApplicationDBcontext db, UserManager<Applicaionuser> user,          SalaryclackServesses SalaryclackServesses)
-        {  
-             _SalaryclackServesses = SalaryclackServesses;
+        public SalaryTransactionServsess(ApplicationDBcontext db, UserManager<Applicaionuser> user, SalaryclackServesses SalaryclackServesses)
+        {
+            _SalaryclackServesses = SalaryclackServesses;
             _user = user;
             _Context = db;
         }
@@ -37,9 +37,9 @@ namespace ReprestoryServess
             DateTime firstDayOfMonth = new DateTime(today.Year, today.Month, 1);
             DateTime lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
 
-        //Example: Retrieve employees whose employment started before or on the first day of the month
-        //     and ended after or on the last day of the month
-         
+            //Example: Retrieve employees whose employment started before or on the first day of the month
+            //     and ended after or on the last day of the month
+
 
 
 
@@ -58,7 +58,7 @@ namespace ReprestoryServess
             {
 
 
-               EntityEntry<SalaryTransaction>? entityadd =  _Context.SalaryTransactions.Add(model);
+                EntityEntry<SalaryTransaction>? entityadd = _Context.SalaryTransactions.Add(model);
 
                 _Context.SaveChanges();
                 _SalaryclackServesses.CalculateSalary(entityadd.Entity.EmployeeId, (decimal)entityadd.Entity.Amount, entityadd.Entity.TransactionDate);
@@ -68,16 +68,16 @@ namespace ReprestoryServess
 
 
 
-       
+
 
 
 
         public void Delete(int id)
         {
 
-            
 
-         var deletedmodel=   GetById(id);
+
+            var deletedmodel = GetById(id);
 
             deletedmodel.isDeleted = SystemEnums.IsDeleted.Deleted;
             Save(deletedmodel);
@@ -89,7 +89,7 @@ namespace ReprestoryServess
 
         public IEnumerable<SalaryTransactionVM> GetAll()
         {
-            var model= _Context.SalaryTransactions.Include(p => p.Employee).Where(p=> p.IsDeleted == SystemEnums.IsDeleted.NotDeleted).Select(p => new SalaryTransactionVM
+            var model = _Context.SalaryTransactions.Include(p => p.Employee).Where(p => p.IsDeleted == SystemEnums.IsDeleted.NotDeleted).Select(p => new SalaryTransactionVM
             {
                 EmployeeName = _user.Users.Where(m => m.Id == p.EmployeeId).Select(p => p.UserName).FirstOrDefault(),
                 //Amount = p.Amount,
@@ -109,17 +109,17 @@ namespace ReprestoryServess
         public SalaryTransactionVM GetById(int id)
         {
             var model =
-                _Context.SalaryTransactions.Where(p=>p.Id==id).Include(p => p.Employee).Where(p => p.IsDeleted == SystemEnums.IsDeleted.NotDeleted).Select(p => new SalaryTransactionVM
+                _Context.SalaryTransactions.Where(p => p.Id == id).Include(p => p.Employee).Where(p => p.IsDeleted == SystemEnums.IsDeleted.NotDeleted).Select(p => new SalaryTransactionVM
                 {
                     EmployeeName = _user.Users.Where(m => m.Id == p.EmployeeId).Select(p => p.UserName).FirstOrDefault(),
 
                     Amount = p.Amount,
-                isDeleted = p.IsDeleted,
-                EmployeeId = p.EmployeeId,
-                TransactionDate = p.TransactionDate,
-                Reason = p.Reason,
-                Id = p.Id,
-                transactionTyp = p.transactionTyp,
+                    isDeleted = p.IsDeleted,
+                    EmployeeId = p.EmployeeId,
+                    TransactionDate = p.TransactionDate,
+                    Reason = p.Reason,
+                    Id = p.Id,
+                    transactionTyp = p.transactionTyp,
 
                 }).FirstOrDefault();
 
@@ -187,7 +187,6 @@ namespace ReprestoryServess
                                propertyValue.Contains(search, comparison);
         }
 
-
-
+       
     }
 }
